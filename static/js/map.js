fbd51569b3834ec2ad8c57d8df176373b4628976
Nam Nguyen <nguyen6626nam@gmail.com>
2024-05-26 20:18:04 +0300

    Added enter and tab functionality

--- static/js/map.js
+++ static/js/map.js
@@ -1,43 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const map = L.map('map').setView([60.18575, 24.82730], 15); // Undergrad coords
+const map = L.map('map', { zoomSnap: 0.5, zoomDelta: 0.5 }).setView([60.18575, 24.82730], 15); // Undergrad coords
+const cycle = ['lat1', 'lng1', 'lat2', 'lng2']
+let points = []
+let markers = []
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+function calculateAndDisplay() {
+    function isNotNumeric(str) {
+        return (isNaN(str) || isNaN(parseFloat(str)))
+    }
 
-    let points = []
-    let markers = []
+    const coords = cycle.map(
+        (id) => document.getElementById(id).value
+    )
+    if (coords.some(isNotNumeric)) {
+        document.getElementById('res').innerHTML = "INVALID INPUT!"
+        return
+    }
 
-    document.getElementById('calcBtn').addEventListener('click', function () {
-        function isNotNumeric(str) {
-            return (isNaN(str) || isNaN(parseFloat(str)))
-        }
+    // Remove old markers, if there's any
+    markers.forEach((marker) => marker.remove())
 
-        const coords = ['lat1', 'lng1', 'lat2', 'lng2'].map(
-            (id) => document.getElementById(id).value
-        )
-        if (coords.some(isNotNumeric)) {
-            document.getElementById('res').innerHTML = "INVALID INPUT!"
-            return
-        }
+    points = [L.latLng(coords.slice(0, 2)), L.latLng(coords.slice(2))]
 
-        // Remove old markers, if there's any
-        markers.forEach((marker) => marker.remove())
-
-        points = [L.latLng(coords.slice(0, 2)), L.latLng(coords.slice(2))]
-
-        markers = points.map(L.marker)
-        markers.forEach((marker, i) => {
-            marker.bindPopup('POSITION ' + (i + 1))
-            marker.addTo(map)
-        })
-
-        map.fitBounds(points)
-        const distInMeter = map.distance(points[0], points[1])
-        const displayDist = (distInMeter > 1000 ? distInMeter / 1000 : distInMeter).toFixed(2)
-        const unit = distInMeter > 1000 ? 'km' : 'm'
-
-        document.getElementById('res').innerHTML = `DISTANCE BETWEEN THE LOCATIONS: ${displayDist} ${unit}`
+    markers = points.map(L.marker)
+    markers.forEach((marker, i) => {
+        marker.bindPopup('POSITION ' + (i + 1)).addTo(map)
     })
+
+    map.fitBounds(L.latLngBounds(points).pad(0.02))
+    const distInMeter = map.distance(points[0], points[1])
+    const displayDist = (distInMeter > 1000 ? distInMeter / 1000 : distInMeter).toFixed(2)
+    const unit = distInMeter > 1000 ? 'km' : 'm'
+
+    document.getElementById('res').innerHTML = `DISTANCE BETWEEN THE LOCATIONS: ${displayDist} ${unit}`
+}
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+document.getElementById('calcBtn').addEventListener('click', calculateAndDisplay)
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter' || event.code === 'NumpadEnter')
+        calculateAndDisplay()
 })
